412a78350715e77ca50d0c00d5b9863d7799fe9f
Martin Nowak <code@dawg.eu>
2015-09-29 02:19:35 +0200

    custom ddox theme

---
+++ README.md
@@ -0,0 +1,6 @@
+# scod - documentation generator
+
+Scod is a clean and lightweight theme for [ddox](https://github.com/rejectedsoftware/ddox).
+Simply use It as drop-in replacement for ddox.
+
+![Example](scod.png)
---
+++ assets/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p public/css
+cat assets/css/{normalize,skeleton,tomorrow,scod}.css | yuglify --terminal --type css > public/css/style.min.css
+mkdir -p public/js
+cat assets/js/{jquery,prettify,scod}.js | yuglify --terminal --type js > public/js/script.min.js
