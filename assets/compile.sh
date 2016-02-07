bfb76d17e36b90eaa4b86f0ea2e81e39f3d567a3
Martin Nowak <code@dawg.eu>
2016-02-07 22:39:06 +0100

    update to ddox w/ libdparse-based syntax highlighting

--- assets/compile.sh
+++ assets/compile.sh
@@ -7,4 +7,4 @@ yuglify=node_modules/.bin/yuglify
 mkdir -p public/css
 cat assets/css/{normalize,skeleton,tomorrow,scod}.css | $yuglify --terminal --type css > public/css/style.min.css
 mkdir -p public/js
-cat assets/js/{prettify,scod}.js | $yuglify --terminal --type js > public/js/script.min.js
+cat assets/js/scod.js | $yuglify --terminal --type js > public/js/script.min.js
